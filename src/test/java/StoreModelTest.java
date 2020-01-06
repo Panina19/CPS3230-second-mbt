9283fd2fc5c35be478cdfc77f52f5ee431691b2f
PANINA19 <gcamilleri1997@gmail.com>
2020-01-06 11:36:31 +0100

    Added guards and actions for each state

--- src/test/java/StoreModelTest.java
+++ src/test/java/StoreModelTest.java
@@ -8,11 +8,16 @@ import nz.ac.waikato.modeljunit.Tester;
 import nz.ac.waikato.modeljunit.coverage.ActionCoverage;
 import nz.ac.waikato.modeljunit.coverage.StateCoverage;
 import nz.ac.waikato.modeljunit.coverage.TransitionPairCoverage;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.FileNotFoundException;
 import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
 
 public class StoreModelTest implements FsmModel {
     private StoreStates modelState;
@@ -20,12 +25,15 @@ public class StoreModelTest implements FsmModel {
     private ScanMaltaStateRels sut;
     private WebDriver browser;
 
+    StoreModelTest(WebDriver browser) {
+        this.browser = browser;
+        sut = new ScanMaltaStateRels(browser);
+    }
     public StoreStates getState() {
         return modelState;
     }
 
     public void reset(boolean b) {
-
         sut.setLoggedOut();
         modelState = StoreStates.LOGGED_OUT;
         isLoggedIn = false;
@@ -39,95 +47,121 @@ public class StoreModelTest implements FsmModel {
             sut = new ScanMaltaStateRels(browser);
         }
     }
-   /* public boolean ringGuard () {
-        return getState().equals(TelephoneSystemStates.ON_HOOK);
-    }
-    public @Action void ring () {
-        sut.incomingCall();
-
-        ringing = true;
-        modelState = TelephoneSystemStates.RINGING;
-
-        Assert.assertEquals("The model's ringing state doesn't match the SUT's state.", ringing, sut.isRinging());
+    @Before
+    public void setup() {
+        String chromeDriverFilePath = "C:/Users/PANINA/Desktop/Software_Testing/web/webtesting1/chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", chromeDriverFilePath);
+        browser = new ChromeDriver();
+        browser.manage().window().maximize();
     }
 
-    public boolean stopRingingGuard () {
-        return getState().equals(TelephoneSystemStates.RINGING);
+    @After
+    public void teardown() { browser.quit(); }
+
+    public boolean loggingInGuard() {
+        return (!getState().equals(StoreStates.LOGGED_IN) && !getState().equals(StoreStates.CHECKING_OUT) && isLoggedOut);
     }
-    public @Action void stopRinging () {
-        sut.stopRinging();
+    public @Action
+    void loggedIn() {
+        sut.setLoggedIn();
+        isLoggedIn = true;
+        isLoggedOut = false;
+        isSearching = false;
+        isAddingToCart = false;
+        isRemovingFromCart = false;
+        isCheckingOut = false;
+        modelState = StoreStates.LOGGED_IN;
 
-        ringing = false;
-        modelState = TelephoneSystemStates.ON_HOOK;
-
-        Assert.assertEquals("The model's ringing state doesn't match the SUT's state.", ringing, sut.isRinging());
-    }
-
-    public boolean answerCallGuard () {
-        return getState().equals(TelephoneSystemStates.RINGING);
-    }
-    public @Action void answerCall () {
-        sut.answerCall();
-
-        ringing = false;
-        onHook = false;
-        inCall = true;
-        modelState = TelephoneSystemStates.IN_CALL;
-
-        Assert.assertEquals("The model's ringing state doesn't match the SUT's state.", ringing, sut.isRinging());
-        Assert.assertEquals("The model's on hook state doesn't match the SUT's state.", onHook, sut.isOnHook());
-        Assert.assertEquals("The model's in call state doesn't match the SUT's state.", inCall, sut.isInCall());
+        assertEquals("", isLoggedIn, sut.isLoggedIn());
     }
 
-    public boolean liftReceiverGuard () {
-        return getState().equals(TelephoneSystemStates.ON_HOOK);
+    public boolean loggingOutGuard() {
+        return (!getState().equals(StoreStates.LOGGED_OUT)) && !(getState().equals(StoreStates.CHECKING_OUT));
     }
-    public @Action void liftReceiver () {
-        sut.liftReceiver();
+    public @Action
+    void loggingOut() {
+        sut.setLoggedOut();
+        isLoggedIn = false;
+        isLoggedOut = true;
+        isSearching = false;
+        isAddingToCart = false;
+        isRemovingFromCart = false;
+        isCheckingOut = false;
+        modelState = StoreStates.LOGGED_OUT;
 
-        onHook = false;
-        dialTone = true;
-        modelState = TelephoneSystemStates.DIAL_TONE;
-
-        Assert.assertEquals("The model's on hook state doesn't match the SUT's state.", onHook, sut.isRinging());
-        Assert.assertEquals("The model's dial tone state doesn't match the SUT's state.", dialTone, sut.isDialTone());
-    }
-
-    public boolean pressNumberSequenceGuard () {
-        return getState().equals(TelephoneSystemStates.DIAL_TONE);
-    }
-    public @Action void pressNumberSequence () {
-        sut.call(123);
-
-        dialTone = false;
-        inCall = true;
-        modelState = TelephoneSystemStates.IN_CALL;
-
-        Assert.assertEquals("The model's dial tone state doesn't match the SUT's state.", dialTone, sut.isDialTone());
-        Assert.assertEquals("The model's in call state doesn't match the SUT's state.", inCall, sut.isInCall());
+        assertEquals("", isLoggedOut, sut.isLoggedOut());
     }
 
-    public boolean hangupReceiverGuard () {
-        return getState().equals(TelephoneSystemStates.DIAL_TONE) || getState().equals(TelephoneSystemStates.IN_CALL);
+    public boolean searchingGuard() {
+        return (!(getState().equals(StoreStates.LOGGED_OUT)) && !(getState().equals(StoreStates.CHECKING_OUT)));
     }
-    public @Action void hangupReceiver () {
-        sut.putDownReceiver();
+    public @Action
+    void searching() {
+        sut.setSearching();
+        isLoggedIn = true;
+        isSearching = true;
+        isAddingToCart = false;
+        isRemovingFromCart = false;
+        isCheckingOut = false;
+        modelState = StoreStates.SEARCHING;
 
-        if (getState() == TelephoneSystemStates.DIAL_TONE) {
-            dialTone = false;
-        } else {
-            inCall = false;
-        }
-        onHook = true;
-        modelState = TelephoneSystemStates.ON_HOOK;
+        assertEquals("", isSearching, sut.isSearching());
+    }
 
-        Assert.assertEquals("The model's dial tone state doesn't match the SUT's state.", dialTone, sut.isDialTone());
-        Assert.assertEquals("The model's in call state doesn't match the SUT's state.", inCall, sut.isInCall());
-        Assert.assertEquals("The model's on hook state doesn't match the SUT's state.", onHook, sut.isOnHook());
-    }*/
+    public boolean addingToCartGuard() {
+        return  (getState().equals(StoreStates.SEARCHING));
+    }
+    public @Action
+    void addingToCart() {
+        sut.setAddingToCart();
+        isLoggedIn = true;
+        isSearching = false;
+        isAddingToCart = true;
+        isRemovingFromCart = false;
+        isCheckingOut = false;
+        modelState = StoreStates.ADDING_TO_CART;
+
+        assertEquals("", isAddingToCart, sut.isAddingToCart());
+    }
+
+    public boolean removingFromCartGuard() {
+        return  (getState().equals(StoreStates.REMOVING_FROM_CART)) ||
+                (getState().equals(StoreStates.ADDING_TO_CART)) ;
+    }
+    public @Action
+    void removingFromCart() {
+        sut.setRemovingFromCart();
+        isLoggedIn=true;
+        isSearching = false;
+        isAddingToCart = false;
+        isRemovingFromCart = true;
+        isCheckingOut = false;
+        modelState = StoreStates.REMOVING_FROM_CART;
+
+        assertEquals("", isRemovingFromCart, sut.isRemovingFromCart());
+    }
+
+    public boolean checkingOutGuard() {
+        return  ((getState().equals(StoreStates.LOGGED_IN) || getState().equals(StoreStates.ADDING_TO_CART)) ||
+                (getState().equals(StoreStates.REMOVING_FROM_CART)) && isLoggedIn);
+    }
+    public @Action
+    void checkingOut() {
+        sut.setCheckedOut();
+        isLoggedIn = true;
+        isLoggedOut = false;
+        isSearching = false;
+        isAddingToCart = false;
+        isRemovingFromCart = false;
+        isCheckingOut = true;
+        modelState = StoreStates.CHECKING_OUT;
+
+        assertEquals("", isCheckingOut, sut.isCheckingOut());
+    }
+
     @Test
     public void ScanStoreSystemModelRunner () throws FileNotFoundException {
-        final Tester tester = new GreedyTester(new StoreModelTest());
+        final Tester tester = new GreedyTester(new StoreModelTest(browser));
         tester.setRandom(new Random());
         final GraphListener graphListener = tester.buildGraph();
         tester.addListener(new StopOnFailureListener());
@@ -135,6 +169,7 @@ public class StoreModelTest implements FsmModel {
         tester.addCoverageMetric(new TransitionPairCoverage());
         tester.addCoverageMetric(new StateCoverage());
         tester.addCoverageMetric(new ActionCoverage());
-        tester.generate(250);tester.printCoverage();
+        tester.generate(250);
+        tester.printCoverage();
     }
 }
